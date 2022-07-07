bec5517b7cf5d1f84f751a4c951bde7b2d1ece14
Lucas Gabriel Eschechola da Silva <lucas.eschechola@outlook.com>
2022-07-06 23:04:38 -0300

    Add viewsets and routers

--- courses/urls.py
+++ courses/urls.py
@@ -1,11 +1,24 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter
 
+from .views import (
+    CourseApiView,
+    CoursesApiView,
+    AverageApiView,
+    AveragesApiView,
+    CourseViewSet,
+    AverageViewSet)
 
-from .views import CourseApiView, CoursesApiView, AverageApiView, AveragesApiView
+router = SimpleRouter()
+router.register('courses', CourseViewSet)
+router.register('averages', AverageViewSet)
 
 urlpatterns = [
     path('courses/', CoursesApiView.as_view(), name='courses'),
+    path('courses/<uuid:course_pk>/', CourseApiView.as_view(), name='course'),
+    path('courses/<uuid:course_pk>/averages', AveragesApiView.as_view(), name='courses_averages'),
+    path('courses/<uuid:course_pk>/average/<uuid:average_pk>/', AverageApiView.as_view(), name='course_average'),
+
     path('averages/', AveragesApiView.as_view(), name='averages'),
-    path('courses/<uuid:pk>/', CourseApiView.as_view(), name='course'),
     path('averages/<uuid:pk>/', AverageApiView.as_view(), name='average')
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,55 @@
 from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework import mixins;
+from rest_framework.generics import get_object_or_404
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import Course, Average
 from .serializers import CourseSerializer, AverageSerializer
+
+"""
+API V1
+
+"""
+# class CourseApiView(APIView):
+#     """
+#     API Courses
+#     """
+
+#     def get(self, request):
+#         courses = Course.objects.all()
+#         serializer = CourseSerializer(courses, many=True)
+#         return Response(serializer.data)
+
+#     def post(self, request):
+#         serializer = CourseSerializer(data = request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+# class AverageApiView(APIView):
+#     """
+#     API Average
+#     """
+    
+#     def get(self, request):
+#         averages = Average.objects.all()
+#         serializer = AverageSerializer(averages, many=True)
+#         return Response(serializer.data)
+
+#     def post(self, request):
+#         serializer = AverageSerializer(data = request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+"""
+API V2
+
+"""
 
 class CoursesApiView(generics.ListCreateAPIView):
     queryset = Course.objects.all()
@@ -17,7 +65,46 @@ class AveragesApiView(generics.ListCreateAPIView):
     queryset = Average.objects.all()
     serializer_class = AverageSerializer
 
+    # get queryset busca uma lista 
+    def get_queryset(self):
+        if self.kwargs.get('course_pk'):
+            return self.queryset.filter(courseId=self.kwargs.get('course_pk'))
+        return self.queryset.all()
 
 class AverageApiView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Average.objects.all()
+    serializer_class = AverageSerializer
+
+    #get object busca somente um objeto
+    def get_object(self):
+        if self.kwargs.get('course_pk'):
+            return get_object_or_404(self.get_queryset(),
+                courseId = self.kwargs.get('course_pk'),
+                pk = self.kwargs.get('average_pk'))
+        return get_object_or_404(self.queryset(), pk=self.kwargs.get('average_pk'))
+
+
+"""
+API V3
+
+"""
+
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+    @action(detail=True, url_path="get-averages", methods=["get"])
+    def get_averages(self, request, pk=None):
+        course = self.get_object()
+        serializer = AverageSerializer(course.averages.all(), many=True)
+        return Response(serializer.data)
+
+
+#remove o destroy impedindo delete
+class AverageViewSet(mixins.CreateModelMixin,
+                    mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.ListModelMixin,
+                    viewsets.GenericViewSet):
     queryset = Average.objects.all()
     serializer_class = AverageSerializer
